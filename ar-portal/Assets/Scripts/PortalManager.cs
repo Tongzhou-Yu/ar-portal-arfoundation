03d5a40c3c30b177a2252cd6df9f6490f8ce86ab
Tongzhou-Yu <45578227+Tongzhou-Yu@users.noreply.github.com>
2020-01-25 16:29:05 +0800

    Add ar-portal Unity Project

---
+++ ar-portal/Assets/Scripts/PortalManager.cs
@@ -0,0 +1,77 @@
+﻿// (c) 2020 Tongzhou Yu
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PortalManager : MonoBehaviour
+{
+
+    public GameObject InnerWorld;
+
+    //This materials matter needs to be optimizated!
+    public Material[] materials;
+
+    private Vector3 camPostionInPortalSpace;
+
+    bool wasInFront;
+    bool inOtherWorld;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        SetMaterials(false);
+
+    }
+
+    void SetMaterials(bool fullRender)
+    {
+        var stencilTest = fullRender ? CompareFunction.NotEqual : CompareFunction.Equal;
+
+        foreach (var mat in materials)
+        {
+            mat.SetInt("_StencilComp", (int)stencilTest);
+        }
+    }
+
+    //Set bidirectional function
+    bool GetIsInFront()
+    {
+        GameObject MainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        camPostionInPortalSpace = transform.InverseTransformPoint(MainCamera.transform.position);
+        return camPostionInPortalSpace.y >= 0.01 ? true : false;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        GameObject MainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        if (collider.transform != MainCamera.transform)
+            return;
+        wasInFront = GetIsInFront();
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider collider)
+    {
+        GameObject MainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        if (collider.transform != MainCamera.transform)
+            return;
+        bool isInFront = GetIsInFront();
+        if ((isInFront && !wasInFront) || (wasInFront && !isInFront))
+        {
+            inOtherWorld = !inOtherWorld;
+            SetMaterials(inOtherWorld);
+        }
+        wasInFront = isInFront;
+
+    }
+
+    private void OnDestroy()
+    {
+        SetMaterials(true);
+    }
+
+}
---
+++ ar-portal/Assets/Shaders/PortalDoor.shader
@@ -0,0 +1,51 @@
+﻿// (c) 2020 Tongzhou Yu
+
+Shader "Custom/PortalDoor"
+{
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+		Zwrite Off
+		ColorMask 0
+		Cull off
+
+		Pass
+		{
+			Stencil{
+				Ref 1
+				Comp always
+				Pass replace
+			}
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				return fixed4(0.0, 0.0, 0.0, 0.0);
+			}
+			ENDCG
+		}
+	}
+}
