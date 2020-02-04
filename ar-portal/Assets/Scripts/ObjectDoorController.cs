5dd44905b527e2e30117f03ec9401c3133eba33c
Tongzhou-Yu <45578227+Tongzhou-Yu@users.noreply.github.com>
2020-02-04 17:51:10 +0800

    Add Interactive animated Door sample scene with Image Tracking feature

---
+++ ar-portal/Assets/Scripts/ObjectDoorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectDoorController : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OpenClose();
+    }
+
+        public void OpenClose()
+    {
+        Animator anim = this.GetComponentInChildren<Animator>();
+        anim.SetTrigger("OpenClose");
+    }
+
+}
