732a9f3f43214e05c39daee1476f927aae03466b
Tongzhou-Yu <tongzhou.yu@network.rca.ac.uk>
2022-04-05 17:32:58 +0800

    Add Interactive Stencil Script can move objects from IN to OUT

--- Assets/Scripts/InteractiveStencilMaterial.cs
+++ Assets/Scripts/InteractiveStencilMaterial.cs
@@ -6,9 +6,12 @@ using UnityEngine.Rendering;
 public class InteractiveStencilMaterial : MonoBehaviour
 {
     bool inOtherWorld;
+    bool hasTriggered;
+
     void Start()
     {
         SetMaterials(false);
+        Debug.Log(hasTriggered);
     }
 
     void SetMaterials(bool fullRender)
@@ -18,13 +21,28 @@ public class InteractiveStencilMaterial : MonoBehaviour
 
     }
 
-    private void OnTriggerEnter(Collider collider)
+    private void OnTriggerEnter(Collider portal)
     {
         //the tag of Portal Gameobject need to be set as Portal 
-        if(collider.tag == "Portal") 
+        if(hasTriggered == false && portal.tag == "Portal") 
         {
             inOtherWorld = !inOtherWorld;
             SetMaterials(inOtherWorld);
+            Debug.Log(hasTriggered);
+        }
+    }
+
+    private void OnTriggerExit(Collider portal)
+    {
+        if(portal.tag == "Portal")
+        {
+            if(hasTriggered == true)
+            {
+                inOtherWorld = !inOtherWorld;
+                SetMaterials(inOtherWorld);
+            }
+            hasTriggered = !hasTriggered;
+            Debug.Log(hasTriggered);
         }
     }
 
